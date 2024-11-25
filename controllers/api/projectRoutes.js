a651cc40ecedd429aa8ae9f237a12516b3d5dc10
Alberto C. Garcia <albertogarcia767@yahoo.com>
2024-11-25 18:32:40 -0500

    changed layout

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -35,4 +35,25 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+//http://localhost:3001/api/projects/13
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.update({
+      ...req.body,
+        where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
